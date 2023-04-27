9e0b841febf00c84ea0a5d718b5124460d3dbb34
DanielleM <dlmedeiro@gmail.com>
2023-04-27 15:44:59 -0700

    update render.com database link

--- src/warApi.js
+++ src/warApi.js
@@ -1,15 +1,15 @@
 import axios from "axios";
 
 // https causes errors when running on local host
-const BASE_URL =
-  "https://warcardgame-backend.onrender.com" || "http://localhost:3001";
+// const BASE_URL = "https://war-backend.onrender.com" || "http://localhost:3001";
+const BASE_URL = "https://war-backend.onrender.com";
 
 class WarApi {
   static async request(endpoint, data = {}, method = "get") {
     console.debug("API Call:", endpoint, data, method);
     // Need to add to catch promise from initial function
     //there are multiple ways to pass an authorization token, this is how you pass it in the header.
-    //this has been provided to show you another way to pass the token. you are only expected to read this code for this project.
+
     const url = `${BASE_URL}/${endpoint}`;
     const params = method === "get" ? data : {};
     const withCredentials = false;
